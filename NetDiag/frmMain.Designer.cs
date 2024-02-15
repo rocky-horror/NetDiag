c172532ef2b562ad7a62a6d8f907c9cfb8f5d869
Jay <chase@dickremoval.pw>
2024-02-15 17:54:38 -0600

    add all the things

---
+++ NetDiag/frmMain.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace NetDiag
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Maximum = 4;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(285, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 47);
+            this.ControlBox = false;
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.Text = "NetDiag";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ NetDiag/frmMain.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace NetDiag
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        readonly string[] domains = new string[] { "google.com", "bing.com", "duckduckgo.com", "youtube.com" };
+        private void killmy_self()
+        {
+            Thread.Sleep(1000);
+            Ping pingReq = new Ping();
+
+            List<long> latencies = new List<long>();
+            int lostPackets = 0;
+            progressBar1.Invoke(new MethodInvoker(() => { progressBar1.Maximum = domains.Length * 4; }));
+
+            for (int i = 0; i < domains.Length; ++i)
+                for (int r = 0; r < 4; ++r)
+                {
+                    try
+                    {
+                        PingReply reply = pingReq.Send(domains[i]);
+                        if (reply.Status != IPStatus.Success)
+                            ++lostPackets;
+                        else
+                            latencies.Add(reply.RoundtripTime);
+                    }
+                    catch
+                    {
+                        ++lostPackets;
+                    }
+
+                    progressBar1.Invoke(new MethodInvoker(() => { ++progressBar1.Value; }));
+                    Thread.Sleep(1000);
+                }
+
+            Invoke(new MethodInvoker(Hide));
+            string networkCondition = "Good";
+            double lossPercentage = ((double)lostPackets / (double)(latencies.Count + lostPackets)) * 100d;
+            if (lossPercentage > 0d)
+                networkCondition = "Poor";
+            else if (lossPercentage > 49d)
+                networkCondition = "Horrible";
+
+            if (latencies.Count < 1)
+            {
+                MessageBox.Show("The diagnostic has completed. There is no internet connection.", "NetDiag",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Invoke(new MethodInvoker(Close));
+            }
+            else
+            {
+                double averageLatency = 0d;
+                foreach (double dubble in latencies)
+                    averageLatency += dubble;
+
+                averageLatency = averageLatency / latencies.Count;
+                if (networkCondition == "Good")
+                {
+                    if (averageLatency < 25d)
+                        networkCondition = "Excellent";
+                    else if (averageLatency > 45d)
+                        networkCondition = "Decent";
+                    else if (averageLatency > 70d)
+                        networkCondition = "Poor";
+                    else if (averageLatency > 120d)
+                        networkCondition = "Horrible";
+                }
+
+                MessageBox.Show("The diagnostic has completed. The results are as follows:\n\n" +
+                    $"Average Latency: {averageLatency}ms.\n" +
+                    $"Loss Percentage: {lossPercentage}%.\n" +
+                    $"Network Condition: {networkCondition}.", "NetDiag", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Invoke(new MethodInvoker(Close));
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            new Thread(killmy_self).Start();
+        }
+    }
+}
